fe8fe9398464ce74609c402a960460db323ed7ac
Adil <61059129+adilce009@users.noreply.github.com>
2025-07-10 11:08:34 -0500

    Create vectorizer_builder.py

---
+++ vectorization/vectorizer_builder.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+from l5kit.configs.config import load_metadata
+from l5kit.data.map_api import MapAPI
+from vectorization.vectorizer import Vectorizer
+#from vectorization import Vectorizer
+
+from l5kit.data import DataManager
+
+
+def build_vectorizer(cfg: dict, data_manager: DataManager) -> Vectorizer:
+    """Factory function for vectorizers, reads the configs, loads required data and initializes the vectorization.
+
+    Args:
+        cfg (dict): Config.
+        data_manager (DataManager): Datamanager that is used to require files to be present.
+
+    Returns:
+        Vectorizer: Vectorizer initialized given the supplied configs.
+    """
+    dataset_meta_key = cfg["raster_params"]["dataset_meta_key"]  # TODO positioning of key
+    dataset_meta = load_metadata(data_manager.require(dataset_meta_key))
+    world_to_ecef = np.array(dataset_meta["world_to_ecef"], dtype=np.float64)
+
+    mapAPI = MapAPI(data_manager.require(cfg["raster_params"]["semantic_map_key"]), world_to_ecef)
+    vectorizer = Vectorizer(cfg, mapAPI)
+
+    return vectorizer
